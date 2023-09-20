67987cb268a271413a06a7b8676926b95bb42dcb
Szyk707 <kolbusszymon@gmail.com>
2023-09-20 12:43:55 +0200

    dziadek ma downa

--- index.php
+++ index.php
@@ -7,9 +7,90 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>lodzie</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sail">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-    <!-- Dałem, żeby nie wpisywać za każdym razem -->
-    <a href="./PHP/admin_panel.php">ADMIN</a>
+    <img src="images/background_sea.png" alt="" class="background_sea">
+
+    <div class="nav">
+        <img src="images/captain.png" alt="captain" class="logo">
+        <p class="title">Boat store</p>
+
+        <div class="navbar">
+            <form action="post" class="search">
+                <input type="text" name="search" class="search_input" placeholder="Search..">
+            </form>
+            <a href="#about_us" class="menu">About us</a>
+            <a href="" class="menu">Contact</a>
+            <a href="" class="menu">Log in</a>
+        </div>
+
+    </div>
+
+
+
+    <div class="content">
+        
+        <div class="side_bar">
+            <h4>Filter the boats you love</h4>
+            <form action="post">
+
+
+
+
+            </form>
+        </div>
+
+
+        <div class="middle">
+            
+            <div class="boats">
+                <p>rqgagaq</p>
+                <p>egegega</p>
+                <p>gqrgqegrq</p>
+                <p>grghtrj</p>
+                <p>e5je</p>
+                <p>rqgagaq</p>
+                <p>egegega</p>
+                <p>gqrgqegrq</p>
+                <p>grghtrj</p>
+                <p>e5je</p>
+                <p>rqgagaq</p>
+                <p>egegega</p>
+                <p>gqrgqegrq</p>
+                <p>grghtrj</p>
+                <p>e5je</p>
+                <p>rqgagaq</p>
+                <p>egegega</p>
+                <p>gqrgqegrq</p>
+                <p>grghtrj</p>
+                <p>e5je</p>
+            </div>
+
+
+            <div class="about_us" id="about_us">
+                <h3>About us</h3>
+                <img src="images/about_us.png" alt="" class="about_us_boat">
+            </div>
+            
+        </div>
+    </div>
+
+
+
+    <div class="footer">
+        <!-- Dałem, żeby nie wpisywać za każdym razem -->
+        <a href="./PHP/admin_panel.php">ADMIN</a>
+    </div>
+</body>
+</html>
 </body>
 </html>
